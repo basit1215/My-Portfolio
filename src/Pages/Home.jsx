b34554596b697b12bf7ad904845bcb30fd47809d
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-10-19 23:54:11 +0500

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -22,11 +22,11 @@ import './Home.css';
 
 const Home = () => {
 
-  const roles = ['Frontend  Developer', 'Web Designer'];
+  const roles = ['Frontend  Developer...', 'Web Designer...'];
   const [currentRoleIndex, setCurrentRoleIndex] = useState(0);
   const [displayedText, setDisplayedText] = useState('');
   const [isDeleting, setIsDeleting] = useState(false);
-  const [typingSpeed, setTypingSpeed] = useState(100); 
+  const [typingSpeed, setTypingSpeed] = useState(100);
 
   useEffect(() => {
     const currentRole = roles[currentRoleIndex];
@@ -34,13 +34,13 @@ const Home = () => {
 
     if (isDeleting) {
       typingTimeout = setTimeout(() => {
-        setDisplayedText((prevText) => prevText.slice(0, -1)); 
-        setTypingSpeed(50); 
+        setDisplayedText((prevText) => prevText.slice(0, -1));
+        setTypingSpeed(50);
       }, typingSpeed);
     } else {
       typingTimeout = setTimeout(() => {
-        setDisplayedText((prevText) => currentRole.slice(0, prevText.length + 1)); 
-        setTypingSpeed(100); 
+        setDisplayedText((prevText) => currentRole.slice(0, prevText.length + 1));
+        setTypingSpeed(100);
       }, typingSpeed);
     }
 
@@ -55,138 +55,108 @@ const Home = () => {
   }, [displayedText, isDeleting, currentRoleIndex, typingSpeed, roles]);
 
 
+
+
   return (
-    <div className="min-h-[80vh] bg-[#031540] w-full flex items-center gap-72 justify-center">
-      <div className="flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-16 p-8 max-w-5xl mx-auto">
-    
-        {/* Text Section */}
-        <div className="text-white text-center md:text-left mr-28 max-w-lg">
-          <h4 className="text-lg mb-2">Hi There<span className="wave" role="img" aria-labelledby="wave">👋🏻</span></h4>
-          <h1 className="mb-4 tracking-wide bg-clip-text text-transparent bg-gradient-to-r from-white to-[#00abf0]">
-            <span className='text-xl font-semibold'>I'm</span> <span className='text-[#00abf0] text-4xl font-bold'>Abdul Basit</span>
+    <div className="min-h-[80vh] bg-[#031540] w-full justify-center">
+      <div className="flex  justify-center md:flex-row  gap-12 md:gap-16 py-[60px]  ml-[5rem] ">
+
+        <div className="w-[420px]  text-white text-center md:text-left py-10 md:w-[50%] md:mr-12">
+          <h4 className="text-[26px] mb-1 tracking-tight font-medium">
+            Hi There <span className="wave" role="img" aria-labelledby="wave"> 👋🏻</span>
+          </h4>
+          <h1 className="mb-4">
+            <span className='text-[42px] font-semibold'>I'm </span>
+            <span className='text-[#00abf0] text-[56px] font-bold'> Abdul Basit</span>
           </h1>
-    
-          <h3 className="text-xl md:text-2xl lg:text-3xl text-blue-600 font-semibold mb-4">
-            <div className="relative">
-              <span className="inline-block whitespace-nowrap overflow-hidden border-r-2 border-black animate-typing pr-10">
-                {displayedText}
-              </span>
-            </div>
-          </h3>
-    
-          {/* Resume Button */}
-          <div className="mt-6">
+          <div className='h-12 '>
+            <h3 className=" md:text-2xl lg:text-3xl tracking-[2px] text-[#00abf0] font-mono">
+              <div className="relative">
+                <span className="text-[35px] inline-block whitespace-nowrap overflow-hidden border-r-2 border-black animate-typing pr-10">
+                  {displayedText}
+                </span>
+              </div>
+            </h3>
+          </div>
+
+          {/* Buttons Section */}
+          <div className="mt-4 flex gap-5">
+
             <a
               href="#"
-              className="inline-block px-6 py-2 hover:px-[25px] hover:py-[9px] bg-blue-500 text-black font-semibold text-base rounded-full border-2 border-transparent duration-300 ease-in-out transition hover:shadow-none transform hover:bg-blue-600 shadow-[#00abf0] shadow-[0_0_15px_var(--main-color)]"
+              className="inline-block px-6 py-2 hover:px-[25px] hover:py-[9px] bg-blue-500 text-[#0e1b38] font-bold text-base rounded-full border-2 border-transparent duration-300 ease-in-out transition hover:shadow-none transform hover:bg-blue-500 shadow-[#00abf0] shadow-[0_0_13px_var(--main-color)]"
             >
               Resume
             </a>
+
+            <a
+              href="#"
+              className="inline-block px-6 mx-2 py-2 text-[#00abf0] font-bold text-base rounded-full border-2 border-blue-500 duration-300 ease-in-out transition-all hover:bg-blue-500 hover:text-[#0e1b38] shadow-none hover:shadow-[#00abf0] hover:shadow-[0_0_13px_var(--main-color)]"
+            >
+              Hire
+            </a>
+
           </div>
         </div>
-    
-        {/* Image Section */}
-        <div className="w-80 h-80 rounded-full ml-24 overflow-hidden border-4 border-white shadow-lg transition-all duration-300 hover:scale-105 mt-8 md:mt-0">
+
+        <div className="w-[150%] md:w-[50%] h-80 overflow-hidden transition-all duration-300 hover:scale-105 md:ml-[11rem]">
           <img
             src={basit}
             alt="basit"
-            className="w-full h-full object-cover"
+            className="h-full rounded-lg"
           />
         </div>
       </div>
     </div>
   );
-  
+
 
 
 
   // return (
-  //   <div className="min-h-[80vh] bg-[#031540] flex items-center justify-center">
-  //     <div className="flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-16 p-8 max-w-5xl mx-auto">
-  
-  //       {/* Text Section */}
-  //       <div className="text-white text-center md:text-left max-w-lg">
-  //         <h4 className="text-lg">Hi There<span className="wave" role="img" aria-labelledby="wave">👋🏻</span></h4>
-  //         <h1 className=" mb-4 tracking-wide bg-clip-text text-transparent bg-gradient-to-r from-white to-[#00abf0]">
-  //           <span className='text-xl font-semibold'>I'm</span> <span className='text-[#00abf0] text-4xl font-bold'>Abdul Basit</span>
+  //   <div className="min-h-[80vh] bg-[#031540] w-full">
+  //     <div className="flex flex-col justify-around md:flex-row items-center gap-12 md:gap-16 py-[103px] max-w-5xl mx-auto">
+
+  //       <div className="w-[420px] mx-10 text-white text-center md:text-left  md:w-[50%] md:mr-12">
+  //         <h4 className="text-[26px] mb-1 tracking-tight font-medium">
+  //           Hi There <span className="wave" role="img" aria-labelledby="wave"> 👋🏻</span>
+  //         </h4>
+  //         <h1 className="mb-4">
+  //           <span className='text-[38px] font-semibold'>I'm </span> 
+  //           <span className='text-[#00abf0] text-[40px] font-bold'> Abdul Basit</span>
   //         </h1>
-  
-  //         <h3 className="text-xl md:text-2xl lg:text-3xl text-blue-600 font-semibold">
-  //           <div className="relative">
-  //             <span className="inline-block whitespace-nowrap overflow-hidden border-r-2 border-black animate-typing pr-2">
-  //               {displayedText}
-  //             </span>
-  //           </div>
-  //         </h3>
-  
-  //         {/* Resume Button */}
-  //         <div className="mt-6">
+  //         <div className='h-12 w-[188px]'>
+  //           <h3 className="text-[22px] md:text-2xl lg:text-3xl text-[#00abf0] font-mono">
+  //             <div className="relative">
+  //               <span className="inline-block whitespace-nowrap overflow-hidden border-r-2 border-black animate-typing pr-10">
+  //                 {displayedText}
+  //               </span>
+  //             </div>
+  //           </h3>
+  //         </div>
+
+  //         <div className="mt-4">
   //           <a
   //             href="#"
-  //             className="inline-block px-6 py-2 hover:px-[25px]  hover:py-[9px] bg-blue-500 text-black font-semibold text-base rounded-full border-2 border-transparent  duration-300 ease-in-out transition  hover:shadow-none transform  hover:bg-blue-600  shadow-[#00abf0] shadow-[0_0_15px_var(--main-color)]"
+  //             className="inline-block px-6 py-2 hover:px-[25px] hover:py-[9px] bg-blue-500 text-[#0e1b38] font-bold text-base rounded-full border-2 border-transparent duration-300 ease-in-out transition hover:shadow-none transform hover:bg-blue-600 shadow-[#00abf0] shadow-[0_0_13px_var(--main-color)]"
   //           >
   //             Resume
   //           </a>
   //         </div>
   //       </div>
-  
-  //       {/* Image Section */}
-  //       <div className="w-80 h-80 rounded-full overflow-hidden border-4 border-white shadow-lg transition-all duration-300 hover:scale-105">
+
+  //       <div className="w-[80%] md:w-[50%] h-80   overflow-hidden transition-all duration-300 hover:scale-105 md:ml-[13rem]">
   //         <img
   //           src={basit}
   //           alt="basit"
-  //           className="w-full h-full object-cover"
+  //           className=" h-full  rounded-lg"
   //         />
   //       </div>
   //     </div>
   //   </div>
   // );
-  
 
 
-  // return (
-  //   <div className="min-h-[80vh] bg-[#031540] flex items-center justify-center">
-  //     <div className="flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-16 p-8 max-w-5xl mx-auto">
-
-  //       <div className="text-white text-center md:text-left max-w-lg">
-  //         <h4>Hi There<span className="wave" role="img" aria-labelledby="wave">👋🏻</span></h4>
-  //         <h1 className="text-5xl font-extrabold mb-4 tracking-wide bg-clip-text text-transparent bg-white text-white">
-  //           I'm <span className='text-[#00abf0]'>Abdul Basit</span>
-  //         </h1>
-
-
-  //         <h3 className="text-xl md:text-2xl lg:text-3xl text-blue-600 font-semibold">
-  //           <div className="relative">
-  //             <span className="inline-block whitespace-nowrap overflow-hidden border-r-2 border-black animate-typing pr-2">
-  //               {displayedText}
-  //             </span>
-  //           </div>
-  //         </h3>
-  
-
-
-
-  //         <div className="bg-[var(--main-color)]  shadow-[0_0_25px_var(--main-color)] p-4">
-  //           <a
-  //             href="#"
-  //             className="inline-block px-6 my-4 py-2 shadow-[#00abf0] shadow-[0_0_25px_var(--main-color)] p-4 bg-blue-500 text-black font-semibold text-base rounded-full border-2 border-transparent transition-transform duration-300 ease-in-out transform hover:scale-110 hover:bg-blue-600 hover:shadow-2xl"
-  //           >
-  //             Resume
-  //           </a>
-  //         </div>
-
-  //       </div>
-
-  //       <div className="w-80 h-80 rounded-full overflow-hidden border-4 border-white shadow-lg transition-all duration-300 hover:scale-105">
-  //         <img
-  //           src={basit}
-  //           alt="basit"
-  //           className="w-full h-full object-cover"
-  //         />
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,43 +15,50 @@
 
 
 import React from 'react';
-import { FaHome, FaUser, FaProjectDiagram, FaFileAlt, FaPhoneAlt, FaStar, FaGithub } from 'react-icons/fa';
+import { FaHome, FaUser, FaProjectDiagram, FaFileAlt, FaPhoneAlt, FaGithub } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#031540] to-black text-white p-4">
+    <nav className="bg-[#031540] to-black text-white py-5 px-[5rem]">
       <div className="container mx-auto flex justify-between items-center">
-        {/* Logo */}
-        <div className="text-2xl font-bold text-purple-400">
-          Abdullah.
+      
+        <div className="text-2xl font-bold text-[#00abf0]">
+          Abdul Basit.
         </div>
-        {/* Links */}
-        <div className="flex space-x-6">
-          <a href="#" className="flex items-center space-x-2 hover:text-purple-400">
+        
+        <div className="flex space-x-9">
+          <a href="#" className="flex items-center space-x-2 hover:text-[#00abf0]">
             <FaHome />
             <span>Home</span>
           </a>
-          <a href="#" className="flex items-center space-x-2 hover:text-purple-400">
+          <a href="#" className="flex items-center space-x-2 hover:text-[#00abf0]">
             <FaUser />
             <span>About</span>
           </a>
-          <a href="#" className="flex items-center space-x-2 hover:text-purple-400">
+          <a href="#" className="flex items-center space-x-2 hover:text-[#00abf0]">
             <FaProjectDiagram />
             <span>Projects</span>
           </a>
-          <a href="#" className="flex items-center space-x-2 hover:text-purple-400">
+
+          {/* <a href="#" className="flex items-center space-x-2 hover:text-[#00abf0]">
             <FaFileAlt />
             <span>Experience</span>
-          </a>
-          <a href="#" className="flex items-center space-x-2 hover:text-purple-400">
+          </a> */}
+
+          <a href="#" className="flex items-center space-x-2 hover:text-[#00abf0]">
             <FaPhoneAlt />
             <span>Contact</span>
           </a>
-        </div>
-        {/* Button */}
-        <button className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded flex items-center space-x-2">
+          <a href='https://github.com/basit1215' target='blank' className="  text-2xl text-white hover:text-black px-2 py-2 hover:rounded-3xl hover:bg-[#00abf0] flex items-center">
           <FaGithub />
-        </button>
+        </a>
+
+        </div>
+       
+        {/* <button className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded flex items-center space-x-2">
+          <FaGithub />
+        </button> */}
+
       </div>
     </nav>
   );
