8bec671a061549b8a877f13850d6d4567685bf91
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-11-09 11:47:13 +0500

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -581,11 +581,11 @@ const Home = () => {
   return (
     <>
    {/* py-10  */}
-      <div className="min-h-[80vh] home lg:gap-[120px] gap-10 justify-center md:gap-20 bg-[#031540] w-full flex flex-col items-center md:flex-row md:pt-[9rem] md:pb-[5rem]">
+      <div className="min-h-[80vh] basitDiv home lg:gap-[120px] gap-10 justify-center md:gap-20 bg-[#031540] w-full flex flex-col items-center md:flex-row md:pt-[9rem] md:pb-[5rem]">
         <div
           data-aos="fade-right"
           data-aos-delay="200"
-          className="w-[70%] sm:w-[50%] md:w-[60%] lg:w-[33%] flex justify-center mb-8 md:mb-0"
+          className="w-[70%] basit sm:w-[50%] md:w-[60%] lg:w-[33%] flex justify-center mb-8 md:mb-0"
         >
           <img src={basit} alt="basit" className="w-[85%] h-[85%] rounded-lg" />
         </div>
@@ -595,17 +595,17 @@ const Home = () => {
           data-aos-delay="400"
           className="text-white px-4 md:px-0 w-full sm:w-2/3 md:w-[44%] lg:w-[40%] flex flex-col  md:items-start  md:text-left"
         >
-          <h4 className="text-xl homeIntro sm:text-2xl md:text-3xl mb-1">Hi There 👋🏻</h4>
-          <h1 className="text-3xl homeIntro sm:text-4xl md:text-5xl mt-4 mb-2 font-bold">
+          <h4 className="text-xl homeIntro sm:text-[14px] md:text-lg mb-1">Hi There 👋🏻</h4>
+          <h1 className="text-3xl homeIntro abdul sm:text-[28px] md:text-3xl mt-4 mb-2 font-bold">
             I'm <span className="text-[#00abf0]">Abdul Basit</span>
           </h1>
           <div className="h-12 homeIntro">
-            <h3 className="text-lg homeIntro sm:text-xl md:text-2xl text-[#00abf0] font-mono">
+            <h3 className="text-lg homeIntro typing sm:text-xl md:text-2xl text-[#00abf0] font-mono">
               <span className="animate-typing overflow-hidden whitespace-nowrap border-r-2 border-gray-400 pr-2">{displayedText}</span>
             </h3>
           </div>
 
-          <div className="mt-4 flex homeLinksDiv  flex-col sm:flex-row justify-center md:justify-start gap-4">
+          <div className="mt-4 flex homeLinksDiv  flex-col sm:flex-row justify-center md:justify-start w-[100%] gap-4">
             <Link
               to="#"
               data-aos="zoom-in"
@@ -618,7 +618,7 @@ const Home = () => {
               to="#"
               data-aos="zoom-in"
               data-aos-delay="600"
-              className="inline-block px-6 py-2 homeLinks text-[#00abf0] font-bold text-sm sm:text-base rounded-[10%] border-2 border-blue-500 duration-300 ease-in-out transition-all hover:bg-blue-500 hover:text-[#0e1b38] shadow-none hover:shadow-[#00abf0] hover:shadow-[0_0_13px_var(--main-color)]"
+              className="inline-block px-6 py-2 homeLinks lg:px-3 lg:text-[16px] text-[#00abf0] font-bold text-[16px] sm:text-base rounded-[10%] border-2 border-blue-500 duration-300 ease-in-out transition-all hover:bg-blue-500 hover:text-[#0e1b38] shadow-none hover:shadow-[#00abf0] hover:shadow-[0_0_13px_var(--main-color)]"
             >
               Contact Me
             </Link>
@@ -626,13 +626,13 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row items-center justify-between bg-[#252c41] text-white py-8 px-4  md:px-20 gap-8 md:gap-0  ">
+      <div className="flex flex-col basitIntroDiv md:flex-row items-center justify-between bg-[#252c41] text-white py-[5rem] px-4  md:px-20 gap-8 md:gap-0  ">
         <div
           data-aos="fade-up"
           data-aos-delay="300"
-          className="w-full  md:w-1/2 lg:w-3/5 lg:pl-[32px] Letme  md:pl-[25px] sm:px-[1rem]"
+          className="w-full  md:w-1/2 lg:w-3/5 allIntro lg:pl-[32px] Letme  md:pl-[25px] sm:px-[1rem]"
         >
-          <h1 className="text-3xl sm:text-4xl md:text-center text-center mySelf md:text-5xl font-bold mb-6">
+          <h1 className="text-[2.5rem] intro sm:text-4xl md:text-center sm:text  font-bold mb-[3.5rem]">
             Let me <span className="text-[#00abf0]">Introduce</span> Myself
           </h1>
           <p className="text-base sm:text-lg leading-relaxed">
